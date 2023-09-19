1ace16f945bbcfb084ad4797e11058544e136498
adama <adhambek.otabekov.post@gmail.com>
2023-09-19 13:01:27 +0200

    working documents plugin

---
+++ src/documents/__init__.py
@@ -0,0 +1,3 @@
+from .main import Documents
+
+def get_plug_class(): return Documents
---
+++ src/documents/main.py
@@ -0,0 +1,97 @@
+from plug.qt import Plug 
+from plug.qt.utils import register
+from tables import Hash, Metadata
+from gizmo.widget import UpDown, InputList
+
+class Documents(Plug):
+
+    special=['return', 'carriage']
+
+    def __init__(
+            self, 
+            app, 
+            *args,
+            special=special,
+            position='right',
+            mode_keys={'command': 'd'},
+            **kwargs):
+
+        super(Documents, self).__init__(
+                app=app, 
+                special=special,
+                position=position, 
+                mode_keys=mode_keys,
+                **kwargs
+                )
+
+        self.hash=Hash()
+        self.meta=Metadata()
+        self.setUI()
+
+    def setup(self):
+
+        super().setup()
+        self.setConnect()
+
+    def setConnect(self):
+
+        self.event_listener.returnPressed.connect(
+                self.open)
+        self.event_listener.carriageReturnPressed.connect(
+                self.open)
+
+    def setUI(self):
+
+        super().setUI()
+        main=InputList(item_widget=UpDown)
+        self.ui.addWidget(main, 'main', main=True)
+        self.ui.main.input.hideLabel()
+
+    def activateUI(self):
+
+       super().activateUI() 
+       self.dlist=self.getList() 
+       self.ui.main.setList(self.dlist)
+
+    @register('t', modes=['command'])
+    def toggle(self): super().toggle()
+
+    def getList(self):
+
+        data=self.hash.getAll()
+        for d in data:
+            d['up']=d['path'].split('/')[-1]
+            meta=self.meta.getRow({'hash':d['hash']})
+            if meta and meta[0]['title']:
+                d['down']=meta[0]['title']
+                d['up']=d['path'].split('/')[-1]
+                d['up_color']='green'
+        return data
+
+    def open(self, focus=False, how='reset', hide=False):
+
+        item=self.ui.main.list.currentItem()
+        if item:
+            path=self.hash.getPath(
+                    item.itemData['hash'])
+            print(path)
+            if path: 
+                self.app.window.display.open(
+                        path, how=how)
+        if hide: 
+            self.delistenWanted.emit()
+
+    def openAndHide(self): 
+
+        if self.activated: 
+            self.open(focus=True, hide=True)
+
+    def openBelow(self): 
+
+        if self.activated: 
+            self.open(focus=True, how='below')
+
+    def openBelowAndHide(self): 
+
+        if self.activated: 
+            self.open(how='below', hide=True)
