ddf73e4ebb7ab9681ea574f677073919c8c35c21
adam <adam@myworld.net>
2023-11-27 18:59:40 +0100

    working leaper

--- src/leaper/main.py
+++ src/leaper/main.py
@@ -1,79 +1,59 @@
 from plug.qt import Plug
 from gizmo.utils import tag
 
+from .utils import Node
+
 class Leaper(Plug):
 
     def setup(self):
 
         self.jumps={}
-        self.indices={}
         super().setup()
-        self.app.handler.viewChanged.connect(
-                self.resetView)
+        self.app.buffer.viewCreated.connect(
+                self.setView)
 
-    def setDisconnect(self):
+    def setView(self, v):
 
-        self.display.itemChanged.disconnect(
-                self.on_itemChanged)
+        c=self.checkProp('canJump', v)
+        if c:
+            v.positionJumped.connect(
+                    self.save)
+            self.jumps[v]=Node()
 
-    def resetView(self, v):
+    def save(self):
 
-        c1=self.checkProp('canMove', v)
-        c2=self.checkProp('positionChanged', v)
-        if c1 and c2:
-            self.reconnect('disconnect')
-            self.view=v
-            self.reconnect()
-
-    def reconnect(self, kind='connect'):
-
-        v = self.view
-        if not v: return
-        f=getattr(v.positionChanged, kind)
-        f(self.saveState)
-
-    def saveState(self):
-
-        v=self.view
-        if not v in self.jumps:
-            self.jumps[v]={}
-            self.indices[v]=0
-
-        jumps=self.jumps.get(v)
-        index=self.indices.get(v)
-        idx=v.item().index()
-        data=(idx, view.getPosition())
-
-        idx_data=jumps.get(index, None)
-        if idx_data and idx_data!=data:
-            s=len(jumps)-1
-            for i in range(s, index, -1):
-                jumps.pop(i)
-
-        jumps[index]=data
-        self.indices[view]=index+1
+        v=self.app.handler.view()
+        c=self.jumps.get(v)
+        d=v.getLocator(kind='position')
+        c.n=Node(p=c, d=d)
+        self.jumps[v]=c.n
 
     @tag('[', modes=['normal'])
-    def jump_prev(self): 
-        self.jump(delta=-1)
+    def prev(self): 
+
+        v=self.app.handler.view()
+        c=self.jumps.get(v, None)
+        v.positionJumped.disconnect(
+                self.save)
+        if c.p and c.p.d:
+            self.jumps[v]=c.p
+            v.openLocator(
+                    data=c.p.d,
+                    kind='position')
+        v.positionJumped.connect(
+                self.save)
 
     @tag(']', modes=['normal'])
-    def jump_next(self): 
-        self.jump(delta=1)
+    def next(self): 
 
-    def jump(self, delta=1):
-
-        view=self.display.currentView()
-        jumps=self.jumps.get(view, None)
-        if jumps:
-            current=self.indices.get(view)
-            if current>len(jumps)-1:
-                current-=1
-            current+=delta
-            data=jumps.get(current)
-            if data:
-                page, (top, left)= data 
-                self.indices[view]=current
-                self.setDisconnect()
-                view.goto(page, left, top) 
-                self.setConnect()
+        v=self.app.handler.view()
+        c=self.jumps.get(v, None)
+        v.positionJumped.disconnect(
+                self.save)
+        if c.n:
+            self.jumps[v]=c.n
+            v.openLocator(
+                    data=c.n.d,
+                    kind='position')
+        v.positionJumped.connect(
+                self.save)
---
+++ src/leaper/utils.py
@@ -0,0 +1,11 @@
+class Node:
+
+    def __init__(
+            self, 
+            d=None, 
+            n=None, 
+            p=None):
+
+        self.d=d
+        self.n=n
+        self.p=p
